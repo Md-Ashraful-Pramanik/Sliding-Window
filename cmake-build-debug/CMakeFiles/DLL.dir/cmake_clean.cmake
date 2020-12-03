696f4d24dbcbbc161022c989fa5e5372322533f3
md-ashraful-pramanik <1605001@ugrad.cse.buet.ac.bd>
2020-12-03 15:13:36 +0600

    Done

---
+++ cmake-build-debug/CMakeFiles/DLL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DLL.dir/dll.c.o"
+  "DLL"
+  "DLL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DLL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/crc2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crc2.dir/crcGenerator.cpp.o"
+  "crc2"
+  "crc2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crc2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
